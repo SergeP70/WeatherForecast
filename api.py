fddcb1b9dc55224c2a8ffbbb2c365d53e115afe7
sergeP70 <serge@pille.be>
2025-01-18 08:22:17 +0100

    retrieving and showing weather icons in columns catch error when no place

--- api.py
+++ api.py
@@ -1,13 +1,10 @@
 # Get data from the open weather map org
 import requests
-from datetime import datetime
 
 API_KEY = 'c132dc7b3fae3f9b558478477e0c8abd'
-url2 = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c132dc7b3fae3f9b558478477e0c8abd'
 
 
-
-def get_data(place, days, kind = None):
+def get_data(place, days):
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric'
     response = requests.get(url).json()
     # 8 measurements per day, only collect days selected
@@ -17,12 +14,10 @@ def get_data(place, days, kind = None):
     dates = []
     kinds = []
     for index in data:
+        dates.append(index['dt_txt'])
         temperatures.append(index['main']['temp'])
-        datum = datetime.fromtimestamp(index['dt']).strftime('%Y-%m-%d')
-        dates.append(datum)
-        kinds.append(index['weather'][0]['main'])
+        kinds.append(index['weather'][0]['icon'])
 
-    print(kinds)
     return dates, temperatures, kinds
 
 
--- main.py
+++ main.py
@@ -3,15 +3,32 @@ import streamlit as st
 import plotly.express as px
 from api import get_data
 
-
+# Design the Title, input and slider
 st.title("Weather Forecast for the next days")
-place = st.text_input("Place:")
+place = st.text_input("Place:", value='London')
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forcasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t, k = get_data(place, days, option)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
-st.plotly_chart(figure)
+# Get the data
+try:
+    dates, temperatures, kinds = get_data(place, days)
+except KeyError:
+    st.error('Place was not found, enter another place')
+else:
+    # Create a temperature plot or display the sky icons
+    if option == 'Temperature':
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
+        st.plotly_chart(figure)
+    else:
+        # Place the sky icons next to each other
+        cols = st.columns(8)
+        cols_index = 0
+        for kind in kinds:
+            with cols[cols_index]:
+                st.image(f'https://openweathermap.org/img/wn/{kind}@2x.png', width=115)
+                cols_index += 1
+                if cols_index >= len(cols):
+                    cols_index = 0
 
