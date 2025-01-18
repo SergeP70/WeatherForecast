150f7243b79d80c44c99baaef922e4293e6a2c9b
sergeP70 <serge@pille.be>
2025-01-18 16:29:11 +0100

    using a lists of dicts instead of values. Refactoring the code

--- api.py
+++ api.py
@@ -7,19 +7,17 @@ API_KEY = 'c132dc7b3fae3f9b558478477e0c8abd'
 def get_data(place, days):
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric'
     response = requests.get(url).json()
-    # 8 measurements per day, only collect days selected
+
+    # 8 measurements per day, only collect the selected days
     data = response['list'][:days*8]
 
-    temperatures = []
-    dates = []
-    kinds = []
+    # Make a list of dicts containing Date, Temperature and Kind, group by date
+    conditions = []
     for index in data:
-        dates.append(index['dt_txt'])
-        temperatures.append(index['main']['temp'])
-        kinds.append(index['weather'][0]['icon'])
-
-    return dates, temperatures, kinds
-
+        conditions.append({'date': index['dt_txt'],
+                           'temperature': index['main']['temp'],
+                           'kind': index['weather'][0]['icon']})
+    return conditions
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -11,23 +11,30 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-# Get the data
+# Get the data and return it as a list of dicts
 try:
-    dates, temperatures, kinds = get_data(place, days)
+    conditions = get_data(place, days)
 except KeyError:
     st.error('Place was not found, enter another place')
 else:
     # Create a temperature plot or display the sky icons
     if option == 'Temperature':
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
+        # Make a list of the dates and the corresponding temperatures
+        # and return this to the X- and Y-axis
+        figure = px.line(x=[condition['date'] for condition in conditions],
+                         y=[condition['temperature'] for condition in conditions],
+                         labels={"x": "Date", "y": "Temperature"})
         st.plotly_chart(figure)
     else:
         # Place the sky icons next to each other
         cols = st.columns(8)
         cols_index = 0
-        for kind in kinds:
+
+        for condition in conditions:
+            kind = condition['kind']
+            img_label = condition['date']
             with cols[cols_index]:
-                st.image(f'https://openweathermap.org/img/wn/{kind}@2x.png', width=115)
+                st.image(f'https://openweathermap.org/img/wn/{kind}@2x.png', width=115, caption=img_label)
                 cols_index += 1
                 if cols_index >= len(cols):
                     cols_index = 0
